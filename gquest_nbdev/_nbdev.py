cc83d3899c6dc713a04147b9935c07c04efa749a
Marcel Barros <mrdbarros@gmail.com>
2020-01-03 20:04:07 -0300

    config for pycharm debugger

--- gquest_nbdev/_nbdev.py
+++ gquest_nbdev/_nbdev.py
@@ -2,16 +2,16 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"eng_stopwords": "00_core.ipynb",
-         "data_path": "00_core.ipynb",
-         "train_data": "00_core.ipynb",
-         "test_data": "00_core.ipynb",
-         "sample_submission": "00_core.ipynb",
-         "testeFunc": "00_core.ipynb"}
+index = {"data_path": "00_encoder.ipynb",
+         "train_data": "00_encoder.ipynb",
+         "test_data": "00_encoder.ipynb",
+         "sample_submission": "00_encoder.ipynb"}
 
-modules = ["core.py"]
+modules = ["encoder.py",
+           "decoder.py"]
+
+doc_url = "https://mrdbarros.github.io/gquest_nbdev/"
 
 git_url = "https://github.com/mrdbarros/gquest_nbdev/tree/master/"
-
 
 def custom_doc_links(name): return None
